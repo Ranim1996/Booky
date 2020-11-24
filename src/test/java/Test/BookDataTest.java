2f9a882e9eb010f73f1a75d7da27b0f37ed19382
Alayoubi,Ranim R <r.alayoubi@student.fontys.nl>
2020-11-24 20:58:03 +0100

    Some unit testing are added

---
+++ src/test/java/Test/BookDataTest.java
@@ -0,0 +1,151 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.Rule;
+import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+import service.model.Book;
+import service.model.BookType;
+import service.model.Language;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookDataTest {
+
+    @Test // test the size of the array
+    public void AddToLanguageList(){
+        List<Language> languages = new ArrayList<>();
+
+        Language l1 = new Language("AR", "Arabic");
+        Language l2 = new Language("EN", "English");
+
+        languages.add(l1);
+        languages.add(l2);
+
+        int size;
+        size = languages.size();
+
+        assertEquals(2, size);
+    }
+
+    @Test // test the size of book array
+    public void AddToBookList(){
+        List<Language> languages = new ArrayList<>();
+        List<Book> books = new ArrayList<>();
+
+        Language l1 = new Language("AR", "Arabic");
+        Language l2 = new Language("EN", "English");
+
+        languages.add(l1);
+        languages.add(l2);
+
+        books.add(new Book(3, "Name3","Author3", BookType.DetectiveandMystery, "Info", LocalDate.now(),l1));
+        books.add(new Book(4, "Name4","Author4", BookType.LitraryFiction, "Info",LocalDate.now(), l2));
+
+        int size;
+        size = books.size();
+
+        assertEquals(2, size);
+    }
+
+    @Test // test get book by language
+    public void getBookByLanguage(){
+        List<Language> languages = new ArrayList<>();
+        List<Book> books = new ArrayList<>();
+
+        Language l1 = new Language("AR", "Arabic");
+        Language l2 = new Language("EN", "English");
+
+        languages.add(l1);
+        languages.add(l2);
+
+        Book b1 = new Book(3, "Name3","Author3", BookType.DetectiveandMystery, "Info", LocalDate.now(),l1);
+        Book b2 = new Book(4, "Name4","Author4", BookType.LitraryFiction, "Info",LocalDate.now(), l2);
+
+        books.add(b1);
+        books.add(b2);
+
+        assertEquals(l1, books.equals(l1));
+    }
+
+    @Test // test get book by type
+    public void getBookByType(){
+        List<Language> languages = new ArrayList<>();
+        List<Book> books = new ArrayList<>();
+
+        Language l1 = new Language("AR", "Arabic");
+        Language l2 = new Language("EN", "English");
+
+        languages.add(l1);
+        languages.add(l2);
+
+        Book b1 = new Book(3, "Name3","Author3", BookType.DetectiveandMystery, "Info", LocalDate.now(),l1);
+        Book b2 = new Book(4, "Name4","Author4", BookType.LitraryFiction, "Info",LocalDate.now(), l2);
+
+        books.add(b1);
+        books.add(b2);
+
+        assertEquals(BookType.DetectiveandMystery, b1.getBookType());
+    }
+
+    @Test // test get book by id
+    public void getBookId(){
+        List<Language> languages = new ArrayList<>();
+        List<Book> books = new ArrayList<>();
+
+        Language l1 = new Language("AR", "Arabic");
+        Language l2 = new Language("EN", "English");
+
+        languages.add(l1);
+        languages.add(l2);
+
+        Book b1 = new Book(3, "Name3","Author3", BookType.DetectiveandMystery, "Info", LocalDate.now(),l1);
+        Book b2 = new Book(4, "Name4","Author4", BookType.LitraryFiction, "Info",LocalDate.now(), l2);
+
+        books.add(b1);
+        books.add(b2);
+
+        assertEquals(3, b1.getId());
+    }
+
+    @Test // test delete book
+    public void deleteBook(){
+
+        List<Language> languages = new ArrayList<>();
+        List<Book> books = new ArrayList<>();
+
+        Language l1 = new Language("AR", "Arabic");
+        Language l2 = new Language("EN", "English");
+
+        languages.add(l1);
+        languages.add(l2);
+
+        Book b1 = new Book(3, "Name3","Author3", BookType.DetectiveandMystery, "Info", LocalDate.now(),l1);
+        Book b2 = new Book(4, "Name4","Author4", BookType.LitraryFiction, "Info",LocalDate.now(), l2);
+
+        books.add(b1);
+        books.add(b2);
+
+        books.remove(b1);
+
+        assertEquals(1, books.size());
+    }
+
+    @Rule // this rule is added to throw exceptions when its needed
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test // test book object must not be null
+    public void getBook(){
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Book Object must not be null");
+
+        Book b = null;
+    }
+
+
+
+}
---
+++ src/test/java/Test/LikeTest.java
@@ -0,0 +1,42 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.Rule;
+import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+import service.model.Like;
+
+public class LikeTest {
+
+    @Test //check whether like info are correct
+    public void NewLikeTest()
+    {
+        Like l = new Like(1,2,3);
+
+        assertEquals(2, l.getBookId());
+        assertEquals(3, l.getUserId());
+    }
+
+    @Rule // this rule is added to throw exceptions when its needed
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test // book id zero
+    public void getBookIdWithZeroValue() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Book id must not be zero");
+
+        Like l= new Like();
+        l.setBookId(0);
+    }
+
+    @Test // user id zero
+    public void getUserIdWithZeroValue() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("User id must not be zero");
+
+        Like l= new Like();
+        l.setUserId(0);
+    }
+}
