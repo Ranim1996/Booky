43108ba840ccf61878d9479e4285e4ebf667bf57
Alayoubi,Ranim R <r.alayoubi@student.fontys.nl>
2021-01-14 14:13:17 +0100

    fixing the chat room service

--- src/main/java/service/Publisher.java
+++ src/main/java/service/Publisher.java
@@ -42,38 +42,5 @@ class Publisher {
         } catch (IOException ex) {
             Logger.getLogger(Publisher.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        System.out.println("Working Directory = " + System.getProperty("user.dir"));
-
-        URI baseUri = UriBuilder.fromUri("http://localhost/").port(9988).build();
-        ResourceConfig resourceConfig = new ResourceConfig(ChatResources.class);
-
-        resourceConfig.packages("myrest");
-        // disable authentication
-        //resourceConfig.register(AuthenticationFilter.class);
-
-
-        // don't start the server yet, wait until the websocket addon has been added
-        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(baseUri, resourceConfig,false);
-
-        // setup static file handler so that we can also serve html pages.
-        StaticHttpHandler staticHandler = new StaticHttpHandler("static");
-        staticHandler.setFileCacheEnabled(false);
-        server.getServerConfiguration().addHttpHandler(staticHandler,"/static/");
-
-        // Create websocket addon
-        WebSocketAddOn webSocketAddOn = new WebSocketAddOn();
-        server.getListeners().forEach(listener -> { listener.registerAddOn(webSocketAddOn);});
-
-        // register my websocket app
-        ChatRoom chatRoom = new ChatRoom();
-        WebSocketEngine.getEngine().register("/ws", "/project", chatRoom);
-
-        // Now start the server
-        server.start();
-
-        // prevent the app from closing
-        System.out.println("Press enter to stop the server...");
-        System.in.read();
     }
 }
